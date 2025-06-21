cda7664f3c2c1225c3da1447c8f0678e4bfeeb48
Apache-R <joseromeroperez1909@gmail.com>
2025-06-21 18:26:27 -0400

    Created player base object, separated agent cubes from playable cubes.

---
+++ AIW_SOCCER_20JUNE25/Assets/Scripts/SoccerAgent.cs
@@ -0,0 +1,165 @@
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+
+public class SoccerAgent : Unity.MLAgents.Agent, ICubeEntity
+{
+    [Header("Movement Attributes")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float jumpForce;
+
+    [Tooltip("Choose which keys this agent should respond to when using Heuristic mode.")]
+    [SerializeField] private ControlScheme controlScheme = ControlScheme.WASD_Arrows;
+
+
+    [Header("References")]
+    [SerializeField] private Ball ball;
+    [SerializeField] private Net objectiveGoal;
+
+    private Vector3 initialPosition;
+    private Rigidbody rigidBody;
+    private bool isGrounded;
+
+    public override void Initialize()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        rigidBody.linearVelocity = Vector3.zero;
+        rigidBody.angularVelocity = Vector3.zero;
+
+        transform.localPosition = new Vector3(Random.Range(-4f, 4f), 0.5f, Random.Range(-4f, 4f));
+        transform.localRotation = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
+
+        ball.transform.localPosition = new Vector3(Random.Range(-3f, 3f), 0.5f, Random.Range(-3f, 3f));
+        ball.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(rigidBody.linearVelocity);
+        sensor.AddObservation(ball.transform.localPosition);
+        sensor.AddObservation(objectiveGoal.transform.localPosition);
+        sensor.AddObservation((ball.transform.localPosition - transform.localPosition).normalized);
+        sensor.AddObservation((objectiveGoal.transform.localPosition - transform.localPosition).normalized);
+        sensor.AddObservation(isGrounded ? 1f : 0f);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        int moveAction = actions.DiscreteActions[0];   // 0 = none, 1 = forward, 2 = backward
+        int rotateAction = actions.DiscreteActions[1]; // 0 = none, 1 = left, 2 = right
+        int jumpAction = actions.DiscreteActions[2];   // 0 = no jump, 1 = jump
+
+        if (moveAction == 1)
+            rigidBody.MovePosition(rigidBody.position + transform.forward * moveSpeed * Time.fixedDeltaTime);
+        else if (moveAction == 2)
+            rigidBody.MovePosition(rigidBody.position - transform.forward * moveSpeed * Time.fixedDeltaTime);
+
+        if (rotateAction == 1)
+            rigidBody.MoveRotation(rigidBody.rotation * Quaternion.Euler(0f, -rotationSpeed * Time.fixedDeltaTime, 0f));
+        else if (rotateAction == 2)
+            rigidBody.MoveRotation(rigidBody.rotation * Quaternion.Euler(0f, rotationSpeed * Time.fixedDeltaTime, 0f));
+
+        if (jumpAction == 1 && isGrounded)
+        {
+            rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        float distanceToBall = Vector3.Distance(transform.localPosition, ball.transform.localPosition);
+        if (distanceToBall < 1.5f)
+            AddReward(0.05f);
+
+        if (Vector3.Distance(ball.transform.localPosition, objectiveGoal.transform.localPosition) < 1.5f)
+        {
+            AddReward(1.0f);
+            EndEpisode();
+        }
+
+        if (transform.localPosition.y < -1f)
+        {
+            AddReward(-1.0f);
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var discreteActions = actionsOut.DiscreteActions;
+
+        switch (controlScheme)
+        {
+            case ControlScheme.WASD_Arrows:
+                discreteActions[0] = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) ? 1 :
+                                     Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) ? 2 : 0;
+
+                discreteActions[1] = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) ? 1 :
+                                     Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) ? 2 : 0;
+
+                discreteActions[2] = Input.GetKey(KeyCode.Space) ? 1 : 0;
+                break;
+
+            case ControlScheme.IJKL_Shift:
+                discreteActions[0] = Input.GetKey(KeyCode.I) ? 1 :
+                                     Input.GetKey(KeyCode.K) ? 2 : 0;
+
+                discreteActions[1] = Input.GetKey(KeyCode.J) ? 1 :
+                                     Input.GetKey(KeyCode.L) ? 2 : 0;
+
+                discreteActions[2] = Input.GetKey(KeyCode.RightShift) ? 1 : 0;
+                break;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.contacts[0].normal.y > 0.5f)
+        {
+            isGrounded = true;
+        }
+    }
+
+    public void ResetPosition(Vector3 initialPosition)
+    {
+        transform.localPosition = initialPosition;
+    }
+
+    public float[] GetMovementAttributes()
+    {
+        return new float[] { moveSpeed, rotationSpeed, jumpForce };
+    }
+
+    public ControlScheme GetControlScheme()
+    {
+        return controlScheme;
+    }
+
+    public void SetControlScheme(ControlScheme newScheme)
+    {
+        controlScheme = newScheme;
+    }
+
+    public void SetMovementAttributes(float? moveSpeed = null, float? rotationSpeed = null, float? jumpForce = null)
+    {
+        if (moveSpeed.HasValue) this.moveSpeed = moveSpeed.Value;
+        if (rotationSpeed.HasValue) this.rotationSpeed = rotationSpeed.Value;
+        if (jumpForce.HasValue) this.jumpForce = jumpForce.Value;
+    }
+
+    public Vector3 GetInitialPosition()
+    {
+        return initialPosition;
+    }
+
+    public GameObject GetGameObject()
+    {
+        return gameObject;
+    }
+}
