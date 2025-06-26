6976ba6a039fc4db84e08bd035d237002b95c2a3
Knbbisprogramming <kaspian.berheim.brudin@email.com>
2025-06-26 20:00:30 +0200

    added a button in main scene to open the train scene

--- AIW_SOCCER/Assets/Scripts/EnterGame.cs
+++ AIW_SOCCER/Assets/Scripts/EnterGame.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
-{
+{   
     public void StartGame()
     {
         Debug.Log("Starting game...");
         
         SceneManager.LoadScene("Test Scene");
+    }
+
+    public void StartTrainingGame(){
+	Debug.Log("Starting train scene...");
+	SceneManager.LoadScene("Train Scene Test");			
     }
 }
